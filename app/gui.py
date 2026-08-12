3c7cf79c7da0b8d52a0ad82db78f4253bbf98e4d
VaibhavSharma2008 <vaibhav.sharma.39872@gmail.com>
2026-08-13 01:05:11 +0530

    Initial commit

---
+++ app/gui.py
@@ -0,0 +1,6 @@
+import tkinter as tk 
+
+def start_app():
+    window=tk.Tk()
+    window.title("AI cold emailing app")
+    window.mainloop()
---
+++ main.py
@@ -0,0 +1,3 @@
+from app import gui
+
+gui.start_app()
